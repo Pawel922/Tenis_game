f0214e32b1dd981b86cbb0500bde3ec55c5bdb47
Pawel Wozniak <pawel.jan.wozniak@gmail.com>
2020-06-03 23:34:25 +0200

    Basic properties of canvas were introduced

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
-  console.log("DOM fully loaded and parsed");
+  const canvas = document.querySelector('canvas');
+  const ctx = canvas.getContext('2d');
+
+  canvas.width = 1000;
+  canvas.height = 500;
+
+  const cw = canvas.width;
+  const ch = canvas.height;
+
+  function table() {
+    ctx.fillStyle = '#009900';
+    ctx.fillRect(0,0,cw,ch);
+  }
+
+  table();
 })
